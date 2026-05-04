9abcd68f2005b06b4941cf8ece5e980539bd9e5e
Anil-kumar9861 <ak7735205@gmail.com>
2026-05-04 23:07:44 +0530

    add leap_year

---
+++ Loop/Leap_year.java
@@ -0,0 +1,13 @@
+class Leap_year{
+    public static void main(String []args){
+
+        int year=2024;
+
+        if((year%4 == 0 && year%100 != 0) || year%400 == 0){
+            System.out.print("it is a leap year");
+        }
+        else{
+            System.out.print("it is not a leap year");
+        }
+    }
+}
