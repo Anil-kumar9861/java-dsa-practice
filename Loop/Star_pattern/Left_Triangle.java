5e398db3a8ff8fc4cf09831f1cb3e5f1089b320c
Anil-kumar9861 <ak7735205@gmail.com>
2026-05-04 23:46:09 +0530

    add Star_Pattern

---
+++ Loop/Star_pattern/Left_Triangle.java
@@ -0,0 +1,17 @@
+class Left_Triangle{
+    public static void main(String []args){
+
+    int row=5;
+
+
+    for(int i=1;i<=row;i++){
+        for(int j=0;j<=row-i;j++){
+
+                System.out.print("* ");
+        }
+        System.out.println("");
+
+    }
+
+    }
+}
---
+++ Loop/Star_pattern/Pyramid.java
@@ -0,0 +1,20 @@
+class Pyramid{
+    public static void main(String []args){
+        
+        int row=5;
+
+        for(int k=0;k<row;k++){
+
+            for(int i=1;i<row-k;i++){
+               System.out.print(" ");
+            }
+
+            for(int j=0;j<=k;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+
+        }
+
+    }
+}
